a5d4ba07870d7796b6f6d279001537810661ba1c
Southstar <Akinbileoluwaseyi2001@gmail.com>
2022-08-09 08:04:25 +0100

    Add files via upload
    first commit

---
+++ honeyPortfolio/honey.js
@@ -0,0 +1,5 @@
+let menu = document.querySelector("#header2");
+let options = document.querySelector('nav.nav');
+ menu.addEventListener("click", function(){
+   options.classList.toggle("active")
+ })
